dd7bfb1f119ef999be4f28601885eeb2cc8fb891
Gian-Caparas <gianjoebert.caparas@cit.edu>
2026-09-21 23:39:25 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.units}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part name={props.part1} units={props.units1} />
+      <Part name={props.part2} units={props.units2} />
+      <Part name={props.part3} units={props.units3} />
     </div>
   )
 }
@@ -15,19 +23,22 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Number of units {props.exercises1 + props.exercises2 + props.exercises3}
+      Total units: {props.units1 + props.units2 + props.units3}
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Web Development'
+  const course = 'BS Information Technology'
+
   const part1 = 'CSIT Subject 1'
-  const exercises1 = 3
+  const units1 = 3
+
   const part2 = 'CSIT Subject 2'
-  const exercises2 = 3
+  const units2 = 3
+
   const part3 = 'CSIT Subject 3'
-  const exercises3 = 3
+  const units3 = 3
 
   return (
     <div>
@@ -35,17 +46,17 @@ const App = () => {
 
       <Content
         part1={part1}
-        exercises1={exercises1}
+        units1={units1}
         part2={part2}
-        exercises2={exercises2}
+        units2={units2}
         part3={part3}
-        exercises3={exercises3}
+        units3={units3}
       />
 
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        units1={units1}
+        units2={units2}
+        units3={units3}
       />
     </div>
   )
